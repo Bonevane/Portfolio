fae6b49bd5e81b357f437d9467d5909cb9d64836
Bonevane <bonevane2009@gmail.com>
2026-09-24 22:15:28 +0500

    Use bonevane.dev as the site address

--- src/data/Site.js
+++ src/data/Site.js
@@ -1,4 +1,4 @@
 // The site's public address. Everything that needs an absolute URL (canonical
 // links, social tags, structured data, sitemap, robots.txt) reads it from here,
 // including index.html via the %SITE_URL% token (replaced at build time).
-export const siteUrl = "https://www.bonevane.dev";
+export const siteUrl = "https://bonevane.dev";
